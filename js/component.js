66cf07ac160253bd3394bfd8c9b25ca9cabc7316
wangtao <1669010607@qq.com>
2020-09-02 17:46:55 +0800

    123

---
+++ js/component.js
@@ -0,0 +1,10 @@
+Vue.component('tep_receive', {
+  props: ['title'],
+  template: '<input value="收文单位" style="width:50px;">'
+});
+
+Vue.component('tep_urgent', {
+  props: ['title'],
+  template: '<input value="缓急" style="width:50px;">'
+});
+&qaokBtlt2,%
